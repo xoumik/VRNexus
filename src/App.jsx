d0f37139734f66b1abe5dc8479e3ce5140069242
xoumik <shoumik21_ug@ece.nits.ac.in>
2024-07-05 17:11:47 +0530

    VRNexus

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import FeatureSection from "./components/FeatureSection";
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Workflow from "./components/Workflow";
+import { useEffect } from "react";
+
+const App = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+};
+export default App;
